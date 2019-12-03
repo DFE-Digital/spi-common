07c7137fed2d030671269679c11d41a6b3a49e59
Matt Middleton <matt.middleton@mtcs.org.uk>
2019-12-03 14:00:06 +0000

    Re-jigging the wrapper factory...

--- src/Dfe.Spi.Common/Dfe.Spi.Common.Logging/Definitions/Factories/ILoggerWrapperFactory.cs
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Logging/Definitions/Factories/ILoggerWrapperFactory.cs
@@ -1,7 +1,6 @@
 ﻿namespace Dfe.Spi.Common.Logging.Definitions.Factories
 {
     using Dfe.Spi.Common.Models;
-    using Microsoft.Extensions.Logging;
 
     /// <summary>
     /// Describes the operations of the <see cref="ILoggerWrapper" /> factory.
@@ -11,17 +10,12 @@
         /// <summary>
         /// Creates an instance of type <see cref="ILoggerWrapper" />.
         /// </summary>
-        /// <param name="logger">
-        /// An instance of type <see cref="ILogger" />.
-        /// </param>
         /// <param name="requestResponseBase">
         /// An instance of <see cref="RequestResponseBase" />.
         /// </param>
         /// <returns>
         /// An instance of type <see cref="ILoggerWrapper" />.
         /// </returns>
-        ILoggerWrapper Create(
-            ILogger logger,
-            RequestResponseBase requestResponseBase);
+        ILoggerWrapper Create(RequestResponseBase requestResponseBase);
     }
 }
--- src/Dfe.Spi.Common/Dfe.Spi.Common.Logging/Factories/LoggerWrapperFactory.cs
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Logging/Factories/LoggerWrapperFactory.cs
@@ -10,13 +10,25 @@
     /// </summary>
     public class LoggerWrapperFactory : ILoggerWrapperFactory
     {
+        private readonly ILogger logger;
+
+        /// <summary>
+        /// Initialises a new instance of the
+        /// <see cref="LoggerWrapperFactory" /> class.
+        /// </summary>
+        /// <param name="logger">
+        /// An instance of type <see cref="ILogger" />.
+        /// </param>
+        public LoggerWrapperFactory(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
         /// <inheritdoc />
-        public ILoggerWrapper Create(
-            ILogger logger,
-            RequestResponseBase requestResponseBase)
+        public ILoggerWrapper Create(RequestResponseBase requestResponseBase)
         {
             LoggerWrapper toReturn = new LoggerWrapper(
-                logger,
+                this.logger,
                 requestResponseBase);
 
             return toReturn;
