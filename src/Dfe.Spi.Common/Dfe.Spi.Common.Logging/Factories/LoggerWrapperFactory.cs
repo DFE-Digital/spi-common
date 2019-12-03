5d4a8b79d71254ad77487472a7f9078bf92ec7d6
Matt Middleton <matt.middleton@mtcs.org.uk>
2019-12-03 11:48:56 +0000

    Basic logging class, to provide custom dimensions in AppInsights through ILogger.

---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Logging/Factories/LoggerWrapperFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace Dfe.Spi.Common.Logging.Factories
+{
+    using Dfe.Spi.Common.Logging.Definitions;
+    using Dfe.Spi.Common.Logging.Definitions.Factories;
+    using Dfe.Spi.Common.Models;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Implements <see cref="ILoggerWrapperFactory" />.
+    /// </summary>
+    public class LoggerWrapperFactory : ILoggerWrapperFactory
+    {
+        /// <inheritdoc />
+        public ILoggerWrapper Create(
+            ILogger logger,
+            RequestResponseBase requestResponseBase)
+        {
+            LoggerWrapper toReturn = new LoggerWrapper(
+                logger,
+                requestResponseBase);
+
+            return toReturn;
+        }
+    }
+}
