e83d3643311e55b51439994556a2fbf26fd7a815
Matt Middleton <matt.middleton@mtcs.org.uk>
2019-12-17 08:00:55 +0000

    HttpErrorBody model

---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Models/HttpErrorBody.cs
@@ -0,0 +1,41 @@
+﻿namespace Dfe.Spi.Common.Models
+{
+    using System.Net;
+
+    /// <summary>
+    /// Common HTTP error body model, to be returned with all non-successful
+    /// status codes. Extends the Azure API management error model.
+    /// </summary>
+    public class HttpErrorBody : ModelsBase
+    {
+        /// <summary>
+        /// Gets or sets an error identifier. The format of this identifier
+        /// is dependent on the local system.
+        /// </summary>
+        public string ErrorIdentifier
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets an error message.
+        /// Inherited from the Azure API management error model.
+        /// </summary>
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the status code.
+        /// Inherited from the Azure API management error model.
+        /// </summary>
+        public HttpStatusCode StatusCode
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Models/ModelsBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Dfe.Spi.Common.Models
+{
+    using Meridian.MeaningfulToString;
+
+    /// <summary>
+    /// Abstract base class for models in the
+    /// <see cref="Dfe.Spi.Common.Models" /> namespace.
+    /// </summary>
+    public abstract class ModelsBase : MeaningfulBase
+    {
+        // Nothing - just acts as a grouping and inherits from MeaingfulBase.
+    }
+}
