a3274ed43bcc502d772e970ecf4cac99a4ae28c5
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-01-21 13:19:07 +0000

    Method for loading schemas for a particular type from the host assembly.

---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Http.Server.UnitTests/TypeExtensionsTests.cs
@@ -0,0 +1,78 @@
+﻿namespace Dfe.Spi.Common.Http.Server.UnitTests
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using NJsonSchema;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TypeExtensionsTests
+    {
+        [Test]
+        public void GetFunctionJsonSchemaAsync_NoSchemaExists_ThrowsFileNotFoundException()
+        {
+            // Arrange
+            Type type = typeof(SomeMethodWithoutSchema);
+            AsyncTestDelegate asyncTestDelegate = null;
+
+            asyncTestDelegate =
+                async () =>
+                {
+                    // Act
+                    await type.GetFunctionJsonSchemaAsync();
+                };
+
+            // Assert
+            Assert.ThrowsAsync<FileNotFoundException>(asyncTestDelegate);
+        }
+
+        [Test]
+        public void GetFunctionJsonSchemaAsync_CorruptSchemaResourceExists_ThrowsSchemaParsingException()
+        {
+            // Arrange
+            Type type = typeof(SomeCorruptSchemaMethod);
+            AsyncTestDelegate asyncTestDelegate = null;
+
+            asyncTestDelegate =
+                async () =>
+                {
+                    // Act
+                    await type.GetFunctionJsonSchemaAsync();
+                };
+
+            // Assert
+            Assert.ThrowsAsync<SchemaParsingException>(asyncTestDelegate);
+        }
+
+        [Test]
+        public async Task GetFunctionJsonSchemaAsync_WellFormedSchemaResourceExists_ReturnsJsonSchema()
+        {
+            // Arrange
+            Type type = typeof(SomeExampleMethod);
+            JsonSchema jsonSchema = null;
+
+            // Act
+            jsonSchema = await type.GetFunctionJsonSchemaAsync();
+
+            // Assert
+            Assert.IsNotNull(jsonSchema);
+        }
+
+        public class SomeExampleMethod
+        {
+            // Nothing - just declaring this type, so it can be used in a test.
+        }
+
+        public class SomeCorruptSchemaMethod
+        {
+            // Nothing - just declaring this type, so it can be used in a test.
+        }
+
+        public class SomeMethodWithoutSchema
+        {
+            // Nothing - just declaring this type, so it can be used in a test.
+        }
+    }
+}
---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Http.Server/SchemaParsingException.cs
@@ -0,0 +1,55 @@
+﻿namespace Dfe.Spi.Common.Http.Server
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using NJsonSchema;
+
+    /// <summary>
+    /// Represents an error raised upon parsing a corrupt/not well formed
+    /// embedded <see cref="JsonSchema" />.
+    /// </summary>
+    [SuppressMessage(
+        "Microsoft.Design",
+        "CA1032",
+        Justification = "This is internally used, and will not be serialised.")]
+    [ExcludeFromCodeCoverage]
+    public class SchemaParsingException : Exception
+    {
+        private new const string Message =
+            "The embedded schema file, \"{0}\", exists, but is either " +
+            "corrupt or not well-formed. Inspect the inner exception for " +
+            "more detail.";
+
+        /// <summary>
+        /// Initialises a new instance of the
+        /// <see cref="SchemaParsingException" /> class.
+        /// </summary>
+        /// <param name="fullName">
+        /// The full name to the embedded schema.
+        /// </param>
+        /// <param name="jsonReaderException">
+        /// An instance of <see cref="JsonReaderException" />.
+        /// </param>
+        public SchemaParsingException(
+            string fullName,
+            JsonReaderException jsonReaderException)
+            : base(
+                  BuildExceptionMessage(fullName),
+                  jsonReaderException)
+        {
+            // Nothing for now...
+        }
+
+        private static string BuildExceptionMessage(string fullName)
+        {
+            string toReturn = string.Format(
+                CultureInfo.InvariantCulture,
+                Message,
+                fullName);
+
+            return toReturn;
+        }
+    }
+}
