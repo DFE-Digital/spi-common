f39d9cba4421c3a2fe8518fe2724d2841b842f7b
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-01-23 14:46:42 +0000

    Fixing the implementation of ICacheProvider.

---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Caching/Caches/CacheProvider.cs
@@ -0,0 +1,47 @@
+﻿namespace Dfe.Spi.Common.Caching.Caches
+{
+    using System.Collections.Generic;
+    using Dfe.Spi.Common.Caching.Definitions;
+
+    /// <summary>
+    /// Implements <see cref="ICacheProvider" />. An in-memory implementation
+    /// of <see cref="ICacheProvider" />.
+    /// </summary>
+    public class CacheProvider : ICacheProvider
+    {
+        private readonly Dictionary<string, object> cache;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="CacheProvider" />
+        /// class.
+        /// </summary>
+        public CacheProvider()
+        {
+            this.cache = new Dictionary<string, object>();
+        }
+
+        /// <inheritdoc />
+        public void AddCacheItem(string key, object cacheItem)
+        {
+            // We should never need to overwrite what's in the cache.
+            if (!this.cache.ContainsKey(key))
+            {
+                // ... so just check that the key doesn't exist.
+                this.cache.Add(key, cacheItem);
+            }
+        }
+
+        /// <inheritdoc />
+        public object GetCacheItem(string key)
+        {
+            object toReturn = null;
+
+            if (this.cache.ContainsKey(key))
+            {
+                toReturn = this.cache[key];
+            }
+
+            return toReturn;
+        }
+    }
+}
