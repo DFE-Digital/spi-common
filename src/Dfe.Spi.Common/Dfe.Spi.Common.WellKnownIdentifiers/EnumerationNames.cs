554bff21aea84c29b722d16d0d46f625ae4e6e40
Simon Bull <simon.bull.84@googlemail.com>
2020-01-29 11:44:59 +0000

    Add well known identifiers package

---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.WellKnownIdentifiers/EnumerationNames.cs
@@ -0,0 +1,23 @@
+﻿namespace Dfe.Spi.Common.WellKnownIdentifiers
+{
+    /// <summary>
+    /// Set of enumeration names used through SPI.
+    /// </summary>
+    public static class EnumerationNames
+    {
+        /// <summary>
+        /// Name of the provider status enumeration.
+        /// </summary>
+        public const string ProviderStatus = "ProviderStatus";
+
+        /// <summary>
+        /// Name of the provider type enumeration.
+        /// </summary>
+        public const string ProviderType = "ProviderType";
+
+        /// <summary>
+        /// Name of the provider sub-type enumeration.
+        /// </summary>
+        public const string ProviderSubType = "ProviderSubType";
+    }
+}
