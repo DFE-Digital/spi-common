7606ae035709b66f5e64369a5f3663ccf821e10d
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-02-03 14:44:58 +0000

    Contexty bits, required for OAuth threading.

---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Http.Server/Definitions/ISpiExecutionContextManager.cs
@@ -0,0 +1,39 @@
+﻿namespace Dfe.Spi.Common.Http.Server.Definitions
+{
+    using System;
+    using Dfe.Spi.Common.Http.Server.Models;
+    using Microsoft.AspNetCore.Http;
+
+    /// <summary>
+    /// Describes the operations of the
+    /// <see cref="Models.SpiExecutionContext" /> manager.
+    /// </summary>
+    public interface ISpiExecutionContextManager
+    {
+        /// <summary>
+        /// Gets the current <see cref="Models.SpiExecutionContext" />.
+        /// </summary>
+        SpiExecutionContext SpiExecutionContext
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Sets the <see cref="Models.SpiExecutionContext" />, based on
+        /// information pulled from the <see cref="HttpRequest" />.
+        /// </summary>
+        /// <param name="headerDictionary">
+        /// An instance of type <see cref="IHeaderDictionary" />.
+        /// </param>
+        void SetContext(IHeaderDictionary headerDictionary);
+
+        /// <summary>
+        /// Sets the internal request id (i.e. custom dimension information)
+        /// against the instance.
+        /// </summary>
+        /// <param name="internalRequestId">
+        /// Guid to use as internal request id.
+        /// </param>
+        void SetInternalRequestId(Guid internalRequestId);
+    }
+}
---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Http.Server/SpiExecutionContextManager.cs
@@ -0,0 +1,97 @@
+﻿namespace Dfe.Spi.Common.Http.Server
+{
+    using System;
+    using System.Linq;
+    using Dfe.Spi.Common.Http.Server.Definitions;
+    using Dfe.Spi.Common.Http.Server.Models;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Net.Http.Headers;
+
+    /// <summary>
+    /// Implements <see cref="ISpiExecutionContextManager" />.
+    /// </summary>
+    public class SpiExecutionContextManager : ISpiExecutionContextManager
+    {
+        private const string BearerAuthorizationPrefix = "Bearer ";
+        private const string InternalRequestIdHeaderName = "X-Internal-Request-Id";
+        private const string ExternalRequestIdHeaderName = "X-External-Request-Id";
+
+        /// <summary>
+        /// Initialises a new instance of the
+        /// <see cref="SpiExecutionContextManager" /> class.
+        /// </summary>
+        public SpiExecutionContextManager()
+        {
+            // Does nothing, for now.
+        }
+
+        /// <inheritdoc />
+        public SpiExecutionContext SpiExecutionContext
+        {
+            get;
+            private set;
+        }
+
+        /// <inheritdoc />
+        public void SetContext(IHeaderDictionary headerDictionary)
+        {
+            if (headerDictionary == null)
+            {
+                throw new ArgumentNullException(nameof(headerDictionary));
+            }
+
+            Guid? internalRequestId = null;
+            if (headerDictionary.ContainsKey(InternalRequestIdHeaderName))
+            {
+                string internalRequestIdStr =
+                    headerDictionary[InternalRequestIdHeaderName].First();
+
+                internalRequestId = Guid.Parse(internalRequestIdStr);
+            }
+
+            string externalRequestId = null;
+            if (headerDictionary.ContainsKey(ExternalRequestIdHeaderName))
+            {
+                externalRequestId =
+                    headerDictionary[ExternalRequestIdHeaderName].First();
+            }
+
+            string identityToken = null;
+            if (headerDictionary.ContainsKey(HeaderNames.Authorization))
+            {
+                string authorizationValue =
+                    headerDictionary[HeaderNames.Authorization].First();
+
+                if (authorizationValue.StartsWith(BearerAuthorizationPrefix, StringComparison.InvariantCulture))
+                {
+                    identityToken = authorizationValue.Replace(
+                        BearerAuthorizationPrefix,
+                        string.Empty);
+                }
+            }
+
+            SpiExecutionContext spiExecutionContext = new SpiExecutionContext()
+            {
+                InternalRequestId = internalRequestId,
+                ExternalRequestId = externalRequestId,
+                IdentityToken = identityToken,
+            };
+
+            this.SpiExecutionContext = spiExecutionContext;
+        }
+
+        /// <inheritdoc />
+        public void SetInternalRequestId(Guid internalRequestId)
+        {
+            if (this.SpiExecutionContext == null)
+            {
+                throw new InvalidOperationException(
+                    $"No context initialised. Make sure to call " +
+                    $"{nameof(this.SetContext)} *prior* to calling this " +
+                    $"method.");
+            }
+
+            this.SpiExecutionContext.InternalRequestId = internalRequestId;
+        }
+    }
+}
