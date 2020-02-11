78ef9ec1515715de65142235b07c6282afaccd84
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-02-11 12:14:47 +0000

    Identifiers for enumerations on the green fields spreadsheet.

--- src/Dfe.Spi.Common/Dfe.Spi.Common.WellKnownIdentifiers/EnumerationNames.cs
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.WellKnownIdentifiers/EnumerationNames.cs
@@ -6,11 +6,6 @@
     public static class EnumerationNames
     {
         /// <summary>
-        /// Name of the provider status enumeration.
-        /// </summary>
-        public const string ProviderStatus = "ProviderStatus";
-
-        /// <summary>
         /// Name of the provider type enumeration.
         /// </summary>
         public const string ProviderType = "ProviderType";
@@ -19,5 +14,25 @@
         /// Name of the provider sub-type enumeration.
         /// </summary>
         public const string ProviderSubType = "ProviderSubType";
+
+        /// <summary>
+        /// Name of the provider status enumeration.
+        /// </summary>
+        public const string ProviderStatus = "ProviderStatus";
+
+        /// <summary>
+        /// Name of the local authority code enumeration.
+        /// </summary>
+        public const string LocalAuthorityCode = "LocalAuthorityCode";
+
+        /// <summary>
+        /// Name of the boarders code enumeration.
+        /// </summary>
+        public const string BoardersCode = "BoardersCode";
+
+        /// <summary>
+        /// Name of the gender of entry enumeration.
+        /// </summary>
+        public const string GenderOfEntry = "GenderOfEntry";
     }
 }
