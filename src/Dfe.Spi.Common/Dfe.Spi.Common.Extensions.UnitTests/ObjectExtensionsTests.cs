48bec8afc24481c958b3a2f78840bf580e62c086
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-02-18 12:35:25 +0000

    Model pruner.

---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Extensions.UnitTests/ObjectExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿namespace Dfe.Spi.Common.Extensions.UnitTests
+{
+    using Dfe.Spi.Common.Extensions;
+    using Dfe.Spi.Models;
+    using Dfe.Spi.Models.Entities;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class ObjectExtensionsTests
+    {
+        [Test]
+        public void SomethingSomething()
+        {
+            // Arrange
+            LearningProvider learningProvider = new LearningProvider()
+            {
+                Name = "SomeCorp Inc. Acadamies, Ltd",
+                Address = new Address()
+                {
+                    AddressLine1 = "SomeCorp House",
+                    AddressLine2 = "123 Example Way",
+                    County = "Leicestershire",
+                    Town = "Enderby",
+                },
+                DfeNumber = "ABC/DEF",
+                Ukprn = 123,
+                Uprn = "4567890",
+                Urn = 7262827,
+                OpenDate = new DateTime(2010, 8, 2),
+            };
+
+            string[] propertiesToInclude = new string[]
+            {
+                null,
+                nameof(LearningProvider.Address),
+                nameof(LearningProvider.Urn),
+                nameof(LearningProvider.OpenDate),
+                string.Empty,
+                "a field that does not exist",
+                nameof(LearningProvider.Name),
+            };
+
+            LearningProvider pruned = null;
+
+            // Act
+            pruned = learningProvider.PruneModel(propertiesToInclude);
+
+            // Assert
+            Assert.IsNotNull(pruned.Name);
+            Assert.IsNotNull(pruned.Address);
+            Assert.IsNull(pruned.DfeNumber);
+            Assert.IsNull(pruned.Ukprn);
+            Assert.IsNull(pruned.Uprn);
+            Assert.IsNotNull(pruned.Urn);
+            Assert.IsNotNull(pruned.OpenDate);
+        }
+    }
+}
---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Extensions/ObjectExtensions.cs
@@ -0,0 +1,85 @@
+﻿namespace Dfe.Spi.Common.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// Contains extension methods for the <see cref="object" /> class.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        private static Dictionary<Type, PropertyInfo[]> typeProperties =
+            new Dictionary<Type, PropertyInfo[]>();
+
+        /// <summary>
+        /// Prunes an existing input <paramref name="model" />, based on the
+        /// supplied <paramref name="propertiesToInclude" />.
+        /// </summary>
+        /// <typeparam name="TModel">
+        /// A model type.
+        /// </typeparam>
+        /// <param name="model">
+        /// An instance of type <typeparamref name="TModel" />.
+        /// </param>
+        /// <param name="propertiesToInclude">
+        /// An array of <see cref="string" /> values, describing the properties
+        /// to include on the output <typeparamref name="TModel" />.
+        /// </param>
+        /// <returns>
+        /// The pruned <typeparamref name="TModel" />.
+        /// </returns>
+        public static TModel PruneModel<TModel>(
+            this TModel model,
+            string[] propertiesToInclude)
+            where TModel : class
+        {
+            TModel toReturn = null;
+
+            // 1) Get back all the properties for the input model type.
+            Type modelType = typeof(TModel);
+
+            PropertyInfo[] propertyInfos = null;
+            if (typeProperties.ContainsKey(modelType))
+            {
+                propertyInfos = typeProperties[modelType];
+            }
+            else
+            {
+                propertyInfos = modelType.GetProperties();
+
+                typeProperties.Add(modelType, propertyInfos);
+            }
+
+            // 2) Create a new empty instance of type TModel.
+            toReturn = Activator.CreateInstance<TModel>();
+
+            string[] fieldsUpper = propertiesToInclude
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Select(x => x.ToUpperInvariant())
+                .ToArray();
+
+            // 3) Cycle through and add set fields via reflection specified in
+            // fields.
+            PropertyInfo propertyInfo = null;
+            foreach (string field in fieldsUpper)
+            {
+                propertyInfo = propertyInfos
+                    .SingleOrDefault(x => x.Name.ToUpperInvariant() == field);
+
+                if (propertyInfo != null)
+                {
+                    // Get the value and...
+                    object currentValue = propertyInfo.GetValue(model);
+
+                    // Copy it.
+                    propertyInfo.SetValue(toReturn, currentValue);
+                }
+            }
+
+            // 4) Return.
+            return toReturn;
+        }
+    }
+}
