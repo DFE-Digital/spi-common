22e64f52ebc0d4de6d6cc750598ebdd2683a55fe
Matt Middleton <matt.middleton@mtcs.org.uk>
2020-03-05 12:46:49 +0000

    Common Azure Storage thingumies.

---
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.AzureStorage/CloudBlobContainerExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace Dfe.Spi.Common.AzureStorage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.WindowsAzure.Storage.Blob;
+
+    /// <summary>
+    /// Contains extension methods for the <see cref="CloudBlobContainer" />
+    /// class.
+    /// </summary>
+    public static class CloudBlobContainerExtensions
+    {
+        /// <summary>
+        /// Creates an effective <c>ListBlobs</c> method from the
+        /// <see cref="CloudBlobContainer.ListBlobsSegmentedAsync(BlobContinuationToken)" />
+        /// method.
+        /// </summary>
+        /// <param name="cloudBlobContainer">
+        /// An instance of <see cref="CloudBlobContainer" />.
+        /// </param>
+        /// <returns>
+        /// An instance of <see cref="IEnumerable{IListBlobItem}" />.
+        /// </returns>
+        public static async Task<IEnumerable<IListBlobItem>> ListBlobsAsync(
+            this CloudBlobContainer cloudBlobContainer)
+        {
+            IEnumerable<IListBlobItem> toReturn = null;
+
+            if (cloudBlobContainer == null)
+            {
+                throw new ArgumentNullException(nameof(cloudBlobContainer));
+            }
+
+            toReturn =
+                await ListBlobsHelper.ListBlobsAsync(
+                    cloudBlobContainer.ListBlobsSegmentedAsync)
+                    .ConfigureAwait(false);
+
+            return toReturn;
+        }
+    }
+}
