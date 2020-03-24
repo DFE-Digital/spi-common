358418ef3b1d470eb8c3a98a5c83ecbbe6eebd9d
Simon Bull <simon.bull.84@googlemail.com>
2020-03-24 11:46:30 +0000

    Add expiration to cache provider

--- src/Dfe.Spi.Common/Dfe.Spi.Common.Caching/Definitions/ICacheProvider.cs
+++ src/Dfe.Spi.Common/Dfe.Spi.Common.Caching/Definitions/ICacheProvider.cs
@@ -1,4 +1,6 @@
-﻿namespace Dfe.Spi.Common.Caching.Definitions
+﻿using System;
+
+namespace Dfe.Spi.Common.Caching.Definitions
 {
     using System.Threading;
     using System.Threading.Tasks;
@@ -26,6 +28,28 @@
         Task AddCacheItemAsync(
             string key,
             object cacheItem,
+            CancellationToken cancellationToken);
+        
+        /// <summary>
+        /// Adds an item to the cache that will expire after a given duration
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <param name="cacheItem">
+        /// The <see cref="object" /> being cached.
+        /// </param>
+        /// <param name="timeToLive">
+        /// The duration the item should live in cache until it expires
+        /// </param>
+        /// <param name="cancellationToken">
+        /// An instance of <see cref="CancellationToken" />.
+        /// </param>
+        /// <returns></returns>
+        Task AddCacheItemAsync(
+            string key,
+            object cacheItem,
+            TimeSpan timeToLive,
             CancellationToken cancellationToken);
 
         /// <summary>
